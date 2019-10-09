820bc595fc8a6e7960f22c9bbab54fc75db1280f
RanjithRaghav <rnjthrwdy@gmail.com>
2019-10-09 17:07:56 +0530

    ranjith

---
+++ src/org/constructor/Array.java
@@ -0,0 +1,27 @@
+package org.constructor;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Array {
+public static void main(String[] args) {
+	String s="hai welcome to java class";
+	String l = s.substring(4,11);
+	System.out.println(l);
+	int a=0;
+	if (a<10) {
+		System.out.println(a);
+	}		
+	 
+	for (int i = 0; i < s.length(); i++) {
+		System.out.print(s.charAt(i));
+		
+	}
+	 
+}	
+
+}
+
